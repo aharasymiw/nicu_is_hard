5d04cbe2a448f2dd9c0a8a67ff442d7aff20518a
Andrew Harasymiw <aharasymiw@gmail.com>
2023-10-30 16:53:40 -0700

    reskin

--- gfg.js
+++ gfg.js
@@ -4,11 +4,14 @@ let move_speed = 3;
 // Gravity constant value 
 let gravity = 0.5;
 
-// Getting reference to the bird element 
-let bird = document.querySelector('.bird');
+// Getting reference to the baby element 
+let baby = document.querySelector('.baby');
 
-// Getting bird element properties 
-let bird_props = bird.getBoundingClientRect();
+// Getting baby element properties 
+let initial_baby_props = baby.getBoundingClientRect();
+console.log('initial_baby_props:', initial_baby_props);
+let baby_props = initial_baby_props;
+
 let background =
     document.querySelector('.background')
         .getBoundingClientRect();
@@ -25,16 +28,16 @@ let score_title =
 let game_state = 'Start';
 
 // Add an eventlistener for key presses 
-document.addEventListener('keydown', (e) => {
+document.addEventListener('touchstart', (e) => {
 
-    // Start the game if enter key is pressed 
-    if (e.key == 'Enter' &&
-        game_state != 'Play') {
+    // Start the game if the screen is tapped
+    if (game_state != 'Play') {
         document.querySelectorAll('.pipe_sprite')
             .forEach((e) => {
                 e.remove();
             });
-        bird.style.top = '40vh';
+        baby_props = initial_baby_props;
+        baby.style.top = '40vh';
         game_state = 'Play';
         message.innerHTML = '';
         score_title.innerHTML = 'Score : ';
@@ -53,38 +56,38 @@ function play() {
         pipe_sprite.forEach((element) => {
 
             let pipe_sprite_props = element.getBoundingClientRect();
-            bird_props = bird.getBoundingClientRect();
+            baby_props = baby.getBoundingClientRect();
 
             // Delete the pipes if they have moved out 
             // of the screen hence saving memory 
             if (pipe_sprite_props.right <= 0) {
                 element.remove();
             } else {
-                // Collision detection with bird and pipes 
+                // Collision detection with baby and pipes 
                 if (
-                    bird_props.left < pipe_sprite_props.left +
+                    baby_props.left < pipe_sprite_props.left +
                     pipe_sprite_props.width &&
-                    bird_props.left +
-                    bird_props.width > pipe_sprite_props.left &&
-                    bird_props.top < pipe_sprite_props.top +
+                    baby_props.left +
+                    baby_props.width > pipe_sprite_props.left &&
+                    baby_props.top < pipe_sprite_props.top +
                     pipe_sprite_props.height &&
-                    bird_props.top +
-                    bird_props.height > pipe_sprite_props.top
+                    baby_props.top +
+                    baby_props.height > pipe_sprite_props.top
                 ) {
 
                     // Change game state and end the game 
-                    // if collision occurs 
+                    // if collision occurs
                     game_state = 'End';
-                    message.innerHTML = 'Press Enter To Restart';
+                    message.innerHTML = 'Tap To Restart';
                     message.style.left = '28vw';
                     return;
                 } else {
                     // Increase the score if player 
                     // has the successfully dodged the 
                     if (
-                        pipe_sprite_props.right < bird_props.left &&
+                        pipe_sprite_props.right < baby_props.left &&
                         pipe_sprite_props.right +
-                        move_speed >= bird_props.left &&
+                        move_speed >= baby_props.left &&
                         element.increase_score == '1'
                     ) {
                         score_val.innerHTML = +score_val.innerHTML + 1;
@@ -99,28 +102,28 @@ function play() {
     }
     requestAnimationFrame(move);
 
-    let bird_dy = 0;
+    let baby_dy = 0;
     function apply_gravity() {
         if (game_state != 'Play') return;
-        bird_dy = bird_dy + gravity;
-        document.addEventListener('keydown', (e) => {
-            if (e.key == 'ArrowUp' || e.key == ' ') {
-                bird_dy = -7.6;
-            }
+        baby_dy = baby_dy + gravity;
+        document.addEventListener('touchstart', (e) => {
+            baby_dy = -7.6;
+
         });
 
-        // Collision detection with bird and 
+        // Collision detection with baby and 
         // window top and bottom 
 
-        if (bird_props.top <= 0 ||
-            bird_props.bottom >= background.bottom) {
+        if (baby_props.top <= 0 ||
+            baby_props.bottom >= background.bottom) {
             game_state = 'End';
-            message.innerHTML = 'Press Enter To Restart';
+            message.innerHTML = 'Tap To Restart';
             message.style.left = '28vw';
             return;
+        } else {
+            baby.style.top = baby_props.top + baby_dy + 'px';
+            baby_props = baby.getBoundingClientRect();
         }
-        bird.style.top = bird_props.top + bird_dy + 'px';
-        bird_props = bird.getBoundingClientRect();
         requestAnimationFrame(apply_gravity);
     }
     requestAnimationFrame(apply_gravity);
@@ -141,17 +144,25 @@ function play() {
             // Calculate random position of pipes on y axis 
             let pipe_posi = Math.floor(Math.random() * 43) + 8;
             let pipe_sprite_inv = document.createElement('div');
-            pipe_sprite_inv.className = 'pipe_sprite';
+            pipe_sprite_inv.className = 'pipe_sprite top';
             pipe_sprite_inv.style.top = pipe_posi - 70 + 'vh';
             pipe_sprite_inv.style.left = '100vw';
+
+            let pipe_text_top = document.createElement('p');
+            pipe_text_top.textContent = randomMorbidityCause();
+            pipe_sprite_inv.appendChild(pipe_text_top);
 
             // Append the created pipe element in DOM 
             document.body.appendChild(pipe_sprite_inv);
             let pipe_sprite = document.createElement('div');
-            pipe_sprite.className = 'pipe_sprite';
+            pipe_sprite.className = 'pipe_sprite bottom';
             pipe_sprite.style.top = pipe_posi + pipe_gap + 'vh';
             pipe_sprite.style.left = '100vw';
             pipe_sprite.increase_score = '1';
+
+            let pipe_text_bottom = document.createElement('p');
+            pipe_text_bottom.textContent = randomMorbidityCause();
+            pipe_sprite.appendChild(pipe_text_bottom);
 
             // Append the created pipe element in DOM 
             document.body.appendChild(pipe_sprite);
@@ -160,4 +171,17 @@ function play() {
         requestAnimationFrame(create_pipe);
     }
     requestAnimationFrame(create_pipe);
-} 
+}
+
+function randomMorbidityCause() {
+    let randomChoice = getRandomInt(0, morbidityCauses.length);
+    return morbidityCauses[randomChoice];
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+let morbidityCauses = ['Hypoxic', 'Hypotensive', 'Hypercapnic', 'Coagulopathic', 'Encephalopathic', 'Bradycardic', 'Apneic', 'Hypothermic'];
